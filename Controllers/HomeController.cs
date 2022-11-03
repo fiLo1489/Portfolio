e2bd5d62dc5c2079477d6a253f14214dcf5fd736
fiLo1489 <filip.zatko.1489@gmail.com>
2022-11-03 18:24:27 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemestralnaPraca.Models;
+using System.Diagnostics;
+
+namespace SemestralnaPraca.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Wedding()
+        {
+            return View();
+        }
+
+        public IActionResult Event()
+        {
+            return View();
+        }
+
+        public IActionResult Car()
+        {
+            return View();
+        }
+
+        public IActionResult Nature()
+        {
+            return View();
+        }
+
+        public IActionResult Other()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
