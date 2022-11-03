cd5084e1e0bd6ad0ec5c2620294ff492b62c5fc1
fiLo1489 <filip.zatko.1489@gmail.com>
2022-11-03 22:53:32 +0100

    Pridanie modelov

---
+++ Models/PhotoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SemestralnaPraca.Models
+{
+    public class PhotoModel
+    {
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string ORIENTATION { get; set; }
+        public string CATEGORY { get; set; }
+    }
+}
