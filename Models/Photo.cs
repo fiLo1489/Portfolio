f2c7600c824ba77371624e73625503e177d38a05
fiLo1489 <filip.zatko.1489@gmail.com>
2022-11-03 19:08:18 +0100

    Migrácia

---
+++ Models/Photo.cs
@@ -0,0 +1,18 @@
+﻿namespace SemestralnaPraca.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public bool ORIENTATION { get; set; }
+        public string CATEGORY { get; set; }
+
+        public Photo(int id, string title, bool orientation, string category)
+        {
+            ID = id;
+            TITLE = title;
+            ORIENTATION = orientation;
+            CATEGORY = category;
+        }
+    }
+}
